a984f951e816edcceaeff52715d1c49a9304c474
VidnyaniU <vidnyani.umathe@gmail.com>
2024-02-25 10:15:12 +0530

    Updated unionOfTwoSortedArrays.cpp
    Added an optimal approach.

--- Array/unionOfTwoSortedArrays.cpp
+++ Array/unionOfTwoSortedArrays.cpp
@@ -27,10 +27,55 @@ vector<int> unionBrute(vector<int> &nums1, vector<int> &nums2)
     return unionArr;
 }
 
-//optimal using two pointers approach
-vector <int > unionOpitmal(vector<int> &nums1, vector<int> &nums2){
-    vector <int> unionAr;
+// optimal using two pointers approach
+vector<int> unionOptimal(vector<int> &nums1, vector<int> &nums2)
+{
+    vector<int> unionAr;
+    int i = 0, j = 0;
+    int n = nums1.size(), m = nums2.size();
 
+    while (i < n && j < m)
+    {
+        // check if element in the first array lesser than the element in the second
+        if (nums1[i] < nums2[j])
+        {
+            // check if its the first element to put in the union or check the previous element of the uniono array if its different
+            if (unionAr.size() == 0 || unionAr.back() != nums1[i])
+            {
+                unionAr.push_back(nums1[i]);
+                i++;
+            }
+        }
+
+        else
+        {
+            if (unionAr.size() == 0 || unionAr.back() != nums2[j])
+            {
+                unionAr.push_back(nums2[j]);
+                j++;
+            }
+        }
+    }
+
+    // leftover elements in nums1 if any
+    while (i < n)
+    {
+        if (unionAr.back() != nums1[i])
+        {
+            unionAr.push_back(nums1[i]);
+            i++;
+        }
+    }
+
+    // leftover elements in nums2 if any
+    while (j < m)
+    {
+        if (unionAr.back() != nums1[i])
+        {
+            unionAr.push_back(nums2[j]);
+            j++;
+        }
+    }
     return unionAr;
 }
 int main()
@@ -38,7 +83,9 @@ int main()
     vector<int> nums1 = {1, 1, 2, 3, 4, 4};
     vector<int> nums2 = {1, 2, 4, 5, 6};
 
-    vector<int> unionA = unionBrute(nums1, nums2);
+    // vector<int> unionA = unionBrute(nums1, nums2);
+    vector<int> unionA = unionOptimal(nums1, nums2);
+
     for (int i = 0; i < unionA.size(); i++)
     {
         cout << unionA[i];
