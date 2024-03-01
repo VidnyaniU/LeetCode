8a3ec7c3048ca37a9a7082ce64a171110ae654d3
VidnyaniU <vidnyani.umathe@gmail.com>
2024-03-01 09:33:33 +0530

    Added productOfArrayExceptSelf.cpp

---
+++ Array/productOfArrExceptSelf.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Product of Array Except Self
+// brute force
+vector<int> productExceptSelfBrute(vector<int> &nums)
+{
+    int n = nums.size();
+
+    vector<int> ans(n);
+
+    return ans;
+}
+// using DP
+vector<int> productExceptSelf(vector<int> &nums)
+{
+
+    int n = nums.size();
+    vector<int> pmul(n);
+    vector<int> posmul(n);
+    vector<int> ans(n);
+    pmul[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        pmul[i] = pmul[i - 1] * nums[i - 1];
+    }
+    posmul[n - 1] = 1;
+    for (int j = n - 2; j >= 0; j--)
+    {
+        posmul[j] = posmul[j + 1] * nums[j + 1];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = pmul[i] * posmul[i];
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> ans = productExceptSelf(nums);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
+
+    return 0;
+}
