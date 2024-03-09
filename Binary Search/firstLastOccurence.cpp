ba95240faf897d4d290b22eef814d4614ba64418
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-03-09 11:16:58 +0530

    Updated firstLastOccurence.cpp

--- Binary Search/firstLastOccurence.cpp
+++ Binary Search/firstLastOccurence.cpp
@@ -25,16 +25,73 @@ vector<int> searchRangeBrute(vector<int> &nums, int target)
     return ans;
 }
 
-// using upper and lower bound
-vector<int> searchRange(vector<int> &nums, int target);
-
 // using plain binary search
-vector<int> searchRange(vector<int> &nums, int target);
+// two times binary search for both first and last
+
+int firstOccur(vector<int> &nums, int target, int n)
+{
+    int low = 0;
+    int high = n - 1;
+    int first = -1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return first;
+}
+int lastOccur(vector<int> &nums, int target, int n)
+{
+    int low = 0;
+    int high = n - 1;
+    int last = -1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int first = firstOccur(nums, target, n);
+    int last = lastOccur(nums, target, n);
+
+    return {first, last};
+}
 
 int main()
 {
-    vector<int> nums = {2, 4, 6, 8, 8, 8, 11, 13};
-    vector<int> ans = searchRangeBrute(nums, 8);
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    // vector<int> ans = searchRangeBrute(nums, 8);
+    vector<int> ans = searchRange(nums, 8);
+
     for (auto it : ans)
     {
         cout << it << " ";
--- Binary Search/upperBound.cpp
+++ Binary Search/upperBound.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int lowerBound(vector<int> &nums, int target, int size)
+int upperBound(vector<int> &nums, int target, int size)
 {
     int ans = size; // hypothetical answer
     int low = 0, high = size - 1;
@@ -27,6 +27,6 @@ int main()
     vector<int> nums = {3, 5, 7, 8, 10};
     int numsLen = nums.size();
     int target = 10;
-    cout << lowerBound(nums, target, numsLen);
+    cout << upperBound(nums, target, numsLen);
     return 0;
 }
