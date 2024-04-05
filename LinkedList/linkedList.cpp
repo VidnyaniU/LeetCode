3844e9570c195648975aa619593ce03da8486d0e
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-04-05 11:08:27 +0530

    Updated linkedList.cpp

--- LinkedList/linkedList.cpp
+++ LinkedList/linkedList.cpp
@@ -18,6 +18,17 @@ public:
         data = data1;
     }
 };
+
+void printLL(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << "-->";
+        head = head->next;
+    }
+
+    cout << "NULL" << endl;
+}
 Node *convertArrToLL(vector<int> &arr)
 {
     Node *head = new Node(arr[0]);
@@ -32,6 +43,7 @@ Node *convertArrToLL(vector<int> &arr)
     return head;
 }
 
+// DELETION IN LL
 Node *deleteFromHead(Node *head)
 {
     if (head == NULL)
@@ -43,30 +55,50 @@ Node *deleteFromHead(Node *head)
 
     return head; // return new head
 }
+
+Node *deleteFromTail(Node *head)
+{
+    // if there is no element or only one element in the linked list
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    Node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;    // deleting the last node
+    temp->next = nullptr; // pointing the second last node to NULL
+
+    return head; // returns head
+}
 int main()
 {
     // Node *head = new Node(2); // object of node
     // cout << head->data << " " << head->next << endl;
 
-    int cnt = 0;
+    // int cnt = 0;
 
     // convert array to linked list
     vector<int> arr = {2, 4, 5, 6};
     Node *head = convertArrToLL(arr);
 
     // traverse the linkedlist
-    Node *temp = head; // DO NOT TAMPER THE HEAD
-    while (temp)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-        cnt++;
-    }
-    cout << endl
-         << "Length of the Linked List :: " << cnt << endl;
+    // Node *temp = head; // DO NOT TAMPER THE HEAD
+    // while (temp)
+    // {
+    //     cout << temp->data << " ";
+    //     temp = temp->next;
+    //     cnt++;
+    // }
+    // cout << endl
+    //      << "Length of the Linked List :: " << cnt << endl;
 
-    Node *newHead = deleteFromHead(head);
-    cout << newHead->data << endl;
+    // Node *newHead = deleteFromHead(head);
+    // cout << newHead->data << endl;
+
+    head = deleteFromTail(head);
+    printLL(head);
 
     return 0;
 }
