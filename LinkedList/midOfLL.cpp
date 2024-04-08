8a39f74874cdc0b12be09fe8f822cff13b0f0ee4
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-04-08 09:19:12 +0530

    Added midOfLL.cpp

---
+++ LinkedList/midOfLL.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+    }
+};
+// Naive approach
+Node *midLL(Node *head)
+{
+    Node *temp = head;
+    int cnt = 0;
+    while (temp != NULL)
+    {
+        cnt++;
+        temp = temp->next;
+    }
+
+    temp = head;
+    int midNode = (cnt / 2) + 1;
+    while (temp != NULL)
+    {
+        midNode = midNode - 1;
+        if (midNode == 0)
+            break;
+        temp = temp->next;
+    }
+    return temp;
+}
