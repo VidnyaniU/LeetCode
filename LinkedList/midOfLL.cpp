b73cb896781068348f0ed6d4254b55f27fc2d6d7
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-04-09 11:29:36 +0530

    Updated midOfLL.cpp

--- LinkedList/midOfLL.cpp
+++ LinkedList/midOfLL.cpp
@@ -40,3 +40,15 @@ Node *midLL(Node *head)
     }
     return temp;
 }
+// Tortoise-Hare approach
+Node *middleNode(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
