e6cdff774633d7d44f649eb2191bad00b20d90cc
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-04-19 11:23:07 +0530

    Added deleteMiddle.cpp

--- LinkedList/deleteMiddle.cpp
+++ LinkedList/deleteMiddle.cpp
@@ -1,6 +1,40 @@
 #include <bits/stdc++.h>
 #include "linkedList.hpp"
 using namespace std;
+// naive
+ListNode *deleteMiddleN(ListNode *head)
+{
+    ListNode *temp = head;
+
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    // find length
+    int len = 0;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    temp = head;
+
+    int res = len / 2;
+    while (temp != NULL)
+    {
+        res--;
+        if (res == 0)
+        {
+            ListNode *delNode = temp->next;
+            temp->next = temp->next->next;
+            delete (delNode);
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+// optimised
 ListNode *deleteMiddle(ListNode *head)
 {
     if (head == NULL || head->next == NULL)
