38319e79c881763686e0de7d53cd813a136d77a3
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-05-03 10:47:55 +0530

    Updated implementationUsingLL.cpp

--- Stack/implementationUsingLL.cpp
+++ Stack/implementationUsingLL.cpp
@@ -1,7 +1,17 @@
-void stackPush(int x) {
-    stackNode * element = new stackNode(x);
-    element -> next = top;
-    top = element;
-    cout << "Element pushed" << "\n";
+#include <bits/stdc++.h>
+#include "linkedList.hpp"
+using namespace std;
+class stackLL
+{
+  int size = 0;
+
+  void stackPush(int x)
+  {
+    ListNode *element = new ListNode(x);
+    element->next = head; // top
+    head = element;
+    cout << "Element pushed"
+         << "\n";
     size++;
   }
+};
