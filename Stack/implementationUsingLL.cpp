fc42e8f6ba081928f44cdc9e7d60b0674b23aaa2
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-05-03 21:41:14 -0400

    Updated implementationUsingLL.cpp

--- Stack/implementationUsingLL.cpp
+++ Stack/implementationUsingLL.cpp
@@ -14,4 +14,15 @@ class stackLL
          << "\n";
     size++;
   }
+int stackPop() {
+    if (top == NULL) {
+      return -1;
+    }
+    int topData = top -> data;
+    stackNode * temp = top;
+    top = top -> next;
+    delete temp;
+    size--;
+    return topData;
+  }
 };
