57c1eb050f02abd73360dfc7c10887ca8704208a
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-05-12 12:32:31 +0530

    Updated checkPalindrome.cpp

--- String/checkPalindrome.cpp
+++ String/checkPalindrome.cpp
@@ -12,16 +12,31 @@ string reverse(string s)
     return s;
 }
 
-bool isPalindrome(string s1)
+// using reverse
+bool isPalindromeRev(string s)
 {
-    if (s1 == reverse(s1))
+    if (s == reverse(s))
         return true;
 
     return false;
 }
+
+// without using reverse
+bool isPalindrome(string s)
+{
+    int start = 0;
+    int end = s.size() - 1;
+    while (start < end)
+    {
+        if (s[start++] != s[end--])
+            return false;
+    }
+    return true;
+}
 int main()
 {
     string s = "abcba";
+    cout << isPalindromeRev(s) << endl;
     cout << isPalindrome(s) << endl;
 
     return 0;
