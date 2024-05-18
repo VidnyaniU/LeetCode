fe95aa4c6f020870d7492dde2636d8d61a516382
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-05-19 01:24:13 +0530

    Updated maxOccurringChar.cpp

--- String/maxOccurringChar.cpp
+++ String/maxOccurringChar.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Given a string str of lowercase alphabets. The task is to find the maximum occurring character in the string str.
+// Given a string s of alphabets. The task is to find the maximum occurring character in the string str.
 // If more than one character occurs the maximum number of time then print the lexicographically smaller characte
 char maxOccurring(string s)
 {
