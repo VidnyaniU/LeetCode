383abd59e9225bba4c3d3c4ed4dc5e695e8b8f05
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-07-10 15:33:21 +0530

    Added crawler log folder

---
+++ POTD/crawlerLogFolder.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// class crawlerLogFolder {
+//     public int minOperations(String[] logs) {
+//         int count =0;
+//         for (String log :logs){
+//             if(log.equals("../"jdk-8u411-linux-aarch64.tar.gz)){
+//                 if(count >0)count--;
+//             }
+//             else if(!log.equals("./"))count++;
+//         }
+//         return count;
+//     }
+   
+// }
+public class crawlerLogFolder {
+    public int minOperations(String[] logs) {
+        int count =0;
+        for (String log :logs){
+            if(log.equals("../"jdk-8u411-linux-aarch64.tar.gz)){
+                if(count >0)count--;
+            }
+            else if(!log.equals("./"))count++;
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        String[] logs = ["d1/","d2/","../","d21/","./"];
+
+        int ans = minOperations(logs);
+        System.out.println(ans);
+    }
+}
