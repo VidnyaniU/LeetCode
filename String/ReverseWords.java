50644ccd2e3b9253c5a1eda8e8de3c407ac6b675
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-07-19 09:39:16 +0530

    Added reverse words

---
+++ String/ReverseWords.java
@@ -0,0 +1,13 @@
+/**
+ * reverseWords
+ */
+public class ReverseWords {
+
+    public String reverseWords(String s) {
+        String[] str = s.trim().split("\\s+");
+        String out = "";
+        for (int i = str.length - 1; i > 0; i--)
+            out += str[i] + " ";
+        return out + str[0];
+    }
+}
