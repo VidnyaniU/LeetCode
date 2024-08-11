2ac472f421713afa0b224c1087ff123689d8ffb0
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-08-11 12:09:25 +0530

    Added decimal to binary

---
+++ Bit Manipulation/decimalToBinary.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void decToBinary(int n)
+{
+    vector<int> res;
+    while (n > 0)
+    {
+
+        res.push_back(n % 2);
+        n = n / 2;
+    }
+
+    reverse(res.begin(), res.end());
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i];
+    }
+}
+int main()
+{
+    int num = 13;
+    decToBinary(num);
+    return 0;
+}
