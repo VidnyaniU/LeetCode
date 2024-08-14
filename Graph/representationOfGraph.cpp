5873ceb8856b88dc7f333c0a34bb5da08de90f13
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-08-14 08:32:18 +0530

    Updated representation of graph

--- Graph/representationOfGraph.cpp
+++ Graph/representationOfGraph.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // using adjacency matrix
-void adjGraph(int n, int m)
+void adjMatrixGraph(int n, int m)
 {
     int adj[n + 1][n + 1] = {0};
     for (int i = 0; i < m; i++)
@@ -10,6 +10,21 @@ void adjGraph(int n, int m)
         cin >> u >> v;
         adj[u][v] = 1;
         adj[v][u] = 1;
+    }
+}
+
+
+
+void adjListGraph(int n, int m)
+{
+
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // for directed graph we won't need this
     }
 }
 int main()
