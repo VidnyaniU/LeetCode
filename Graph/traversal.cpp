a6ecd277de641f794d1a9430443e7e016e709182
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-08-18 10:49:06 +0530

    Updated traversal
    Added dfs traversal

--- Graph/traversal.cpp
+++ Graph/traversal.cpp
@@ -31,3 +31,28 @@ vector<int> bfsTraversal(int v, vector<int> adj[]) // v is number of vertices an
 
     return bfs;
 }
+
+// DFS traversal
+void dfs(int node, vector<int> adj[], vector<int> &ls, int vis[])
+{
+    vis[node] = 1;
+    ls.push_back(node);
+    for (auto it : adj[node])
+    {
+
+        if (!vis[it])
+        {
+            dfs(it, adj, ls, vis);
+        }
+    }
+}
+// Function to return a list containing the DFS traversal of the graph.
+vector<int> dfsOfGraph(int V, vector<int> adj[])
+{
+    // Code here
+    int vis[V] = {0};
+    int start = 0;
+    vector<int> ls;
+    dfs(start, adj, ls, vis);
+    return ls;
+}
