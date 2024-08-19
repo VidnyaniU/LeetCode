e18595ccfd35750624347d7f7ec552ed87a88bb3
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-08-19 10:05:26 +0530

    Updated representation of graph

--- Graph/representationOfGraph.cpp
+++ Graph/representationOfGraph.cpp
@@ -12,9 +12,7 @@ void adjMatrixGraph(int n, int m)
         adj[v][u] = 1;
     }
 }
-
-
-
+//using adjacency list
 void adjListGraph(int n, int m)
 {
 
@@ -27,8 +25,19 @@ void adjListGraph(int n, int m)
         adj[v].push_back(u); // for directed graph we won't need this
     }
 }
-int main()
-{
 
-    return 0;
+// adj Matrix to adj list
+void adjMatrixToList(vector<vector<int>> adjMatrix, vector<int> adjList[], int V)
+{
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            if (adjMatrix[i][j] == 1 && i != j)
+            {
+                adjList[i].push_back(j);
+                adjList[j].push_back(i);
+            }
+        }
+    }
 }
