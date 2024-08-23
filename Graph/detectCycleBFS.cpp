ab81dd03199b0c9ac7c0543096eccf3cbc6744b7
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-08-23 11:03:04 +0530

    updated detect  cycle
    added dfs approach

--- Graph/detectCycleBFS.cpp
+++ Graph/detectCycleBFS.cpp
@@ -29,6 +29,27 @@ bool detectCycle(int src, vector<int> adj[], int vis[])
 
     return false;
 }
+
+//dfs approach
+bool dfs(int node, int parent, int vis[], vector<int> adj[])
+{
+    vis[node] = 1;
+    // visit adjacent nodes
+    for (auto adjacentNode : adj[node])
+    {
+        // unvisited adjacent node
+        if (!vis[adjacentNode])
+        {
+            if (dfs(adjacentNode, node, vis, adj) == true)
+                return true;
+        }
+        // visited node but not a parent node
+        else if (adjacentNode != parent)
+            return true;
+    }
+    return false;
+}
+
 // Function to detect cycle in an undirected graph.
 bool isCycle(int V, vector<int> adj[])
 {
